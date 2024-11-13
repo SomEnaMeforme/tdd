9bbe39abf0539205435e34e1ca48e90d8daa609d
SomEnaMeforme <rushkova909@gmail.com>
2024-11-13 23:43:21 +0500

    Add CircularCloudLayoter

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        private List<Rectangle> rectanglesLocation = new ();
+        private readonly Point center;
+        private BruteForceNearestFinder nearestFinder;
+
+        public CircleLayer CurrentLayer { get; private set; }
+
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+            nearestFinder = new BruteForceNearestFinder();
+        }
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            Rectangle resultRectangle;
+            if (IsFirstRectangle())
+            {
+                CreateFirstLayer(rectangleSize);
+                resultRectangle = PutRectangleToCenter(rectangleSize);
+            }
+            else
+            {
+                var possiblePosition = CurrentLayer.CalculateTopLeftRectangleCornerPosition(rectangleSize);
+                resultRectangle = new Rectangle(possiblePosition, rectangleSize);
+            }
+            OnSuccessInsertion(resultRectangle);
+            return resultRectangle;
+        }
+
+        private void OnSuccessInsertion(Rectangle r)
+        {
+            rectanglesLocation.Add(r);
+            nearestFinder.Insert(r);
+            if (IsNotFirstInsertion())
+                CurrentLayer.OnSuccessInsertRectangle(r);
+        }
+
+        private void CreateFirstLayer(Size firstRectangle)
+        {
+            var radius = Math.Ceiling(Math.Max(firstRectangle.Width, firstRectangle.Height) / 2.0);
+            CurrentLayer = new CircleLayer(center, (int)radius);
+        }
+
+        private Rectangle PutRectangleToCenter(Size rectangleSize)
+        {
+            var rectangleX = center.X - rectangleSize.Width / 2;
+            var rectangleY = center.Y - rectangleSize.Height / 2;
+
+            return new Rectangle(new Point(rectangleX, rectangleY), rectangleSize);
+        }
+        
+        private bool IsFirstRectangle()
+        {
+            return rectanglesLocation.Count == 0;
+        }
+
+        private bool IsNotFirstInsertion()
+        {
+            return rectanglesLocation.Count > 1;
+        }
+
+        public IEnumerable<Rectangle> GetRectangles()
+        {
+            foreach (var rectangle in rectanglesLocation)
+            {
+                yield return rectangle;
+            }
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouterTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization.Tests
+{
+    public class CircularCloudLayouterTests
+    {
+        private CircularCloudLayouter layouter;
+        private Point defaultCenter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            defaultCenter = new Point(5, 5);
+            layouter = new CircularCloudLayouter(defaultCenter);
+        }
+
+        [Test]
+        public void GetRectangles_ShouldBeEmpty_BeforePutAnyRectangles()
+        {
+            layouter.GetRectangles()
+                .Should().BeEmpty();
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldAddRectangleToCenter_WhenRectangleFirst()
+        {
+            var firstRectangleSize = new Size(6, 4);
+            var expected = new Rectangle(new Point(2, 3), firstRectangleSize);
+
+            var nextRectangle = layouter.PutNextRectangle(firstRectangleSize);
+
+            nextRectangle
+                .Should().Be(expected);
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldCreateFirstCircleLayer_AfterPutFirstRectangle()
+        {
+            var firstRectangleSize = new Size(6, 4);
+
+            layouter.PutNextRectangle(firstRectangleSize);
+
+            layouter.CurrentLayer.Should().NotBeNull();
+        }
+
+        [TestCase(6, 4, 3)]
+        [TestCase(4, 6, 3)]
+        [TestCase(2, 2, 1)]
+        [TestCase(5, 9, 5)]
+        public void PutNextRectangle_ShouldCreateFirstCircleLayer_WithRadiusEqualHalfMaxSizeOfFirstRectangleRoundToInt(int height, int width, int expected)
+        {
+            var firstRectangleSize = new Size(width, height);
+
+            layouter.PutNextRectangle(firstRectangleSize);
+
+            layouter.CurrentLayer.Radius.Should().Be(expected);
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldAddRectangleToLayouter_AfterPut()
+        {
+            var firstRectangleSize = new Size(4, 4);
+
+            layouter.PutNextRectangle(firstRectangleSize);
+
+            layouter.GetRectangles()
+                .Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldUseCircleLayer_ForChoosePositionForRectangle()
+        {
+            var firstRectangleSize = new Size(4, 4);
+            var expected = new Point(5, -1);
+
+            layouter.PutNextRectangle(firstRectangleSize);
+            var secondRectangleLocation = layouter.PutNextRectangle(firstRectangleSize).Location;
+
+            secondRectangleLocation.Should().Be(expected);
+        }
+    }
+}
