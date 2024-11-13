8cae61117ebfed19d7799c105c226d9ca42e8abf
SomEnaMeforme <rushkova909@gmail.com>
2024-11-13 06:37:32 +0500

    AddNearestFinder

---
+++ cs/TagsCloudVisualization/BruteForceNearestFinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public class BruteForceNearestFinder
+    {
+        private List<Rectangle> rectangles = new();
+
+        public void Insert(Rectangle r)
+        {
+            if (RectangleHasInсorrectSize(r))
+                throw new ArgumentException($"Rectangle has incorrect size: width = {r.Width}, height = {r.Height}");
+            rectangles.Add(r);
+        }
+
+        public Rectangle? FindNearestByDirection(Rectangle r, Direction direction)
+        {
+            if (RectangleHasInсorrectSize(r))
+                throw new ArgumentException($"Rectangle has incorrect size: width= {r.Width}, height={r.Height}");
+            if (rectangles.Count == 0)
+                return null;
+            var calculator = GetMinDistanceCalculatorBy(direction);
+            return rectangles.Select(currentRectangle => (distance: calculator(currentRectangle, r), CurrentEl: currentRectangle))
+                .Where(el => el.distance > 0)
+                .MinBy(el => el.distance).CurrentEl;
+        }
+
+        private Func<Rectangle, Rectangle, int> GetMinDistanceCalculatorBy(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Left: return (possibleNearest, rectangleForFind) => rectangleForFind.X - possibleNearest.X;
+                case Direction.Right: return (possibleNearest, rectangleForFind) => possibleNearest.X - rectangleForFind.X;
+                case Direction.Top: return (possibleNearest, rectangleForFind) => rectangleForFind.Y - possibleNearest.Y;
+                default: return (possibleNearest, rectangleForFind) => possibleNearest.Y - rectangleForFind.Y;
+            }
+        }
+
+        private bool RectangleHasInсorrectSize(Rectangle r)
+        {
+            return r.Width <= 0 || r.Height <= 0;
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/BruteForceNearestFinderTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using System.Drawing;
+
+namespace TagsCloudVisualization.Tests
+{
+    public class BruteForceNearestFinderTests
+    {
+        private BruteForceNearestFinder finder;
+        [SetUp]
+        public void SetUp()
+        {
+            finder = new BruteForceNearestFinder();
+        }
+        [Test]
+        public void FindNearest_ShouldReturnNull_BeforeAnyInsertions()
+        {
+            var rectangleForFind = new Rectangle(new Point(5, 7), new Size(4, 2));
+
+            finder.FindNearestByDirection(rectangleForFind, Direction.Top).Should().BeNull();
+        }
+
+        [TestCase(0, 4, TestName = "WhenWidthZero")]
+        [TestCase(3, 0, TestName = "WhenHeightZero")]
+        [TestCase(-3, 4, TestName = "WhenWidthIsNegative")]
+        [TestCase(3, -4, TestName = "WhenHeightNegative")]
+        [TestCase(-3, -4, TestName = "WhenWidthAndHeightNegative")]
+        [TestCase(0, 0, TestName = "WhenWidthAndHeightIsZero")]
+        public void Insert_ShouldThrow(int width, int height)
+        {
+            var rectangleForInsert = new Rectangle(new Point(2, 2), new Size(width, height));
+
+            ShouldThrow((finder, rectangle) => finder.Insert(rectangle), rectangleForInsert);
+        }
+
+        [TestCase(0, 4, TestName = "WhenWidthZero")]
+        [TestCase(3, 0, TestName = "WhenHeightZero")]
+        [TestCase(-3, 4, TestName = "WhenWidthIsNegative")]
+        [TestCase(3, -4, TestName = "WhenHeightNegative")]
+        [TestCase(-3, -4, TestName = "WhenWidthAndHeightNegative")]
+        [TestCase(0, 0, TestName = "WhenWidthAndHeightIsZero")]
+        public void FindNearest_ShouldThrow(int width, int height)
+        {
+            var rectangleForFind = new Rectangle(new Point(2, 2), new Size(width, height));
+
+            ShouldThrow((finder, rectangle) => finder.FindNearestByDirection(rectangle, Direction.Top), rectangleForFind);
+        }
+
+        public void ShouldThrow(Action<BruteForceNearestFinder, Rectangle> callFinderMethod, Rectangle incorrectRectangle)
+        {
+            Action act = () => callFinderMethod(finder, incorrectRectangle);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase(4, 10, Direction.Top)]
+        [TestCase(2, 7, Direction.Top, true)]
+        [TestCase(2, 7, Direction.Right)]
+        public void FindNearest_ShouldReturnNearestRectangleByDirection_ForArgumentRectangle(int x, int y, Direction direction, bool isFirstRectNearest = false)
+        {
+            var addedRectangle1 = new Rectangle(new Point(2, 2), new Size(3, 4));
+            var addedRectangle2 = new Rectangle(new Point(5, 7), new Size(4, 2));
+            var rectangleForFind = new Rectangle(new Point(x, y), new Size(2, 1));
+
+            finder.Insert(addedRectangle1);
+            finder.Insert(addedRectangle2);
+
+            finder.FindNearestByDirection(rectangleForFind, direction).Should().Be(isFirstRectNearest ? addedRectangle1 : addedRectangle2);
+        }
+    }
+}
