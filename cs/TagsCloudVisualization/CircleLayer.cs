09cf278332f3af80ee2d86f91acf01ca6f220a91
SomEnaMeforme <rushkova909@gmail.com>
2024-11-13 23:42:01 +0500

    Add CircularLayer

---
+++ cs/TagsCloudVisualization/CircleLayer.cs
@@ -0,0 +1,193 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class CircleLayer
+{
+    public enum Sector
+    {
+        Top_Right,
+        Bottom_Right,
+        Bottom_Left,
+        Top_Left
+    }
+
+    public Point Center { get; }
+    public int Radius { get; }
+
+
+    private Sector currentSector;
+    private readonly List<Rectangle> layerRectangles;
+    private CircleLayer prevLayer;
+
+    public CircleLayer(Point center, int radius)
+    {
+        Center = center;
+        Radius = radius;
+        currentSector = Sector.Top_Right;
+        layerRectangles = new List<Rectangle>();
+    }
+
+    public CircleLayer OnSuccessInsertRectangle(Rectangle inserted)
+    {
+        currentSector = currentSector == Sector.Top_Left ? Sector.Top_Right : currentSector + 1;
+        layerRectangles.Add(inserted);
+        if (currentSector == Sector.Top_Right) 
+            return CreateNextLayer();
+        return this;
+    }
+
+    private CircleLayer CreateNextLayer()
+    {
+        var nextLayer = new CircleLayer(Center, CalculateRadiusForNextLayer());
+        nextLayer.prevLayer = this;
+        return nextLayer;
+    }
+
+    private int CalculateRadiusForNextLayer()
+    {
+        var prevSector = Sector.Top_Right - 1;
+        return layerRectangles.Select(r => CalculateDistanceBetweenCenterAndRectangleBySector(r, prevSector + 1)).Max();
+    }
+
+    private int CalculateDistanceBetweenCenterAndRectangleBySector(Rectangle r, Sector s)
+    {
+        switch (s)
+        {
+            case Sector.Top_Right: 
+                return CalculateDistanceBetweenPoints(Center, new Point(r.X + r.Width, r.Y));
+            case Sector.Bottom_Right:
+                return CalculateDistanceBetweenPoints(Center, new Point(r.X + r.Width, r.Y + r.Height));
+            case Sector.Bottom_Left:
+                return CalculateDistanceBetweenPoints(Center, new Point(r.X - r.Width, r.Y + r.Height));
+            default: 
+                return CalculateDistanceBetweenPoints(Center, new Point(r.X - r.Width, r.Y));
+        }
+    }
+
+    private int CalculateDistanceBetweenPoints(Point p1, Point p2)
+    {
+        return (int)Math.Ceiling(Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y)));
+    }
+
+    public Point CalculateTopLeftRectangleCornerPosition(Size rectangleSize)
+    {
+        var relevantForSectorPosition = GetPositionRelevantForSector();
+        switch (currentSector)
+        {
+            case Sector.Top_Right:
+                return new Point(relevantForSectorPosition.X, relevantForSectorPosition.Y - rectangleSize.Height);
+            case Sector.Bottom_Right: 
+                return relevantForSectorPosition;
+            case Sector.Bottom_Left:
+                return new Point(relevantForSectorPosition.X - rectangleSize.Width, relevantForSectorPosition.Y);
+            default:
+                return new Point(relevantForSectorPosition.X - rectangleSize.Width,
+                    relevantForSectorPosition.Y - rectangleSize.Height);
+        }
+    }
+
+    private Point GetPositionRelevantForSector()
+    {
+        switch (currentSector)
+        {
+            case Sector.Top_Right: 
+                return new Point(Center.X, Center.Y - Radius);
+            case Sector.Bottom_Right: 
+                return new Point(Center.X + Radius, Center.Y);
+            case Sector.Bottom_Left: 
+                return new Point(Center.X, Center.Y + Radius);
+            default: 
+                return new Point(Center.X - Radius, Center.Y);
+        }
+    }
+
+    public Point GetRectanglePositionWithoutIntersection(Rectangle forInsertion, Rectangle intersected)
+    {
+        return CalculateNewPositionWithoutIntersectionBySector(currentSector, forInsertion, intersected);
+    }
+
+    private Point CalculateNewPositionWithoutIntersectionBySector(Sector s, Rectangle forInsertion, Rectangle intersected)
+    {
+        var distanceForMoving = CalculateDistanceForMovingBySector(s, forInsertion, intersected);
+        var isMovingAxisIsX = IsMovingAxisIsXBySector(s);
+        var nearestForCenterCorner =
+            CalculateCornerNearestForCenterAfterMove(s, distanceForMoving, forInsertion);
+        var distanceForBringBackOnCircle =
+            CalculateDeltaForBringRectangleBackOnCircle(nearestForCenterCorner, isMovingAxisIsX);
+        distanceForMoving *= CalculateMoveMultiplierForMoveClockwise(isMovingAxisIsX, forInsertion);
+        distanceForBringBackOnCircle *= CalculateMoveMultiplierForMoveFromCenter(!isMovingAxisIsX, forInsertion);
+        return isMovingAxisIsX 
+            ? new Point(forInsertion.X + distanceForMoving, forInsertion.Y + distanceForBringBackOnCircle) 
+            : new Point(forInsertion.X + distanceForBringBackOnCircle, forInsertion.Y + distanceForMoving);
+    }
+
+    private int CalculateDeltaForBringRectangleBackOnCircle(Point nearestForCenterCorner, bool isMovingAxisIsX)
+    {
+        Func<Point, int> getAxisForBringBackOnCircle = isMovingAxisIsX ? p => p.Y : p => p.X;
+        Func<Point, int> getStaticAxis = isMovingAxisIsX ? p => p.X : p => p.Y;
+
+        var distanceOnStaticAxis = Math.Abs(getStaticAxis(nearestForCenterCorner) - getStaticAxis(Center));
+        var distanceOnAxisForBringBackOnCircle = Math.Abs(getAxisForBringBackOnCircle(nearestForCenterCorner) - getAxisForBringBackOnCircle(Center));
+        return (int)Math.Ceiling(Math.Sqrt(Radius * Radius - distanceOnStaticAxis * distanceOnStaticAxis)) - distanceOnAxisForBringBackOnCircle;
+    }
+
+    private Point CalculateCornerNearestForCenterAfterMove(Sector s, int distanceForMoving, Rectangle r)
+    {
+        var isAxisForMoveIsX = IsMovingAxisIsXBySector(s);
+        var moveMultiplier = CalculateMoveMultiplierForMoveClockwise(isAxisForMoveIsX, r);
+        distanceForMoving *= moveMultiplier;
+        var nearestCorner = GetCornerNearestForCenterBySector(s, r);
+        return isAxisForMoveIsX 
+            ? new Point(nearestCorner.X + distanceForMoving, nearestCorner.Y) 
+            : new Point(nearestCorner.X, nearestCorner.Y + distanceForMoving);
+    }
+
+    private int CalculateMoveMultiplierForMoveFromCenter(bool isAxisForMoveIsX, Rectangle r)
+    {
+        return isAxisForMoveIsX 
+            ? r.Right < Center.X ? -1 : 1 
+            : r.Bottom < Center.Y ? -1 : 1;
+    }
+    private int CalculateMoveMultiplierForMoveClockwise(bool isAxisForMoveIsX, Rectangle r)
+    {
+        return isAxisForMoveIsX
+            ? r.Left > Center.X ? -1 : 1
+            : r.Bottom > Center.Y ? -1 : 1;
+    }
+
+    private int CalculateDistanceForMovingBySector (Sector s, Rectangle forInsertion, Rectangle intersected)
+    {
+        switch (s)
+        {
+            case Sector.Top_Right:
+                return Math.Abs(forInsertion.Top - intersected.Bottom);
+            case Sector.Bottom_Right:
+                return Math.Abs(forInsertion.Right - intersected.Left);
+            case Sector.Bottom_Left:
+                return Math.Abs(forInsertion.Bottom - intersected.Top);
+            default:
+                return Math.Abs(forInsertion.Left - intersected.Right);
+        }
+    }
+
+    private Point GetCornerNearestForCenterBySector(Sector s, Rectangle r)
+    {
+        switch (s)
+        {
+            case Sector.Top_Right:
+                return new Point(r.Left, r.Bottom);
+            case Sector.Bottom_Right:
+                return new Point(r.Left, r.Top);
+            case Sector.Bottom_Left:
+                return new Point(r.Right, r.Top);
+            default:
+                return new Point(r.Right, r.Bottom);
+        }
+    }
+
+    private bool IsMovingAxisIsXBySector(Sector s)
+    {
+        return s == Sector.Bottom_Right || s == Sector.Top_Left;
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/CircleLayerTests.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using static TagsCloudVisualization.CircleLayer;
+
+namespace TagsCloudVisualization.Tests
+{
+    public class CircleLayerTests
+    {
+        private CircleLayer currentLayer;
+        private Size defaultRectangleSize;
+        [SetUp]
+        public void SetUp()
+        {
+            var startRadius = 5;
+            var center = new Point(5, 5);
+            currentLayer = new CircleLayer(center, startRadius);
+            defaultRectangleSize = new Size(3, 4);
+        }
+
+        [Test]
+        public void CircleLayer_InsertFirstForLayerRectangle_InTopRightSectorStart()
+        {
+            var possibleRectangleLocation = currentLayer.CalculateTopLeftRectangleCornerPosition(defaultRectangleSize);
+
+            possibleRectangleLocation.Should().Be(GetCorrectRectangleLocationByExpectedSector(Sector.Top_Right));
+        }
+
+        [TestCase(1, Sector.Bottom_Right)]
+        [TestCase(2, Sector.Bottom_Left)]
+        [TestCase(3, Sector.Top_Left)]
+        public void CircleLayer_InsertRectangleInNextSector_AfterSuccessInsertion(int insertionsCount, Sector expected)
+        {
+            currentLayer = GetLayerAfterFewInsertionsRectangleWithSameSize(currentLayer, insertionsCount);
+
+            var possibleRectangleLocation = currentLayer.CalculateTopLeftRectangleCornerPosition(defaultRectangleSize);
+
+            possibleRectangleLocation.Should().Be(GetCorrectRectangleLocationByExpectedSector(expected));
+        }
+
+        [Test]
+        public void CircleLayer_GetNewLayer_AfterInsertionsOnAllSectors()
+        {
+            currentLayer = GetLayerAfterFewInsertionsRectangleWithSameSize(currentLayer, 3);
+
+            var nextLayer = currentLayer.OnSuccessInsertRectangle(new Rectangle(new Point(0, 0), defaultRectangleSize));
+
+            nextLayer.Should().NotBeSameAs(currentLayer);
+        }
+
+        [Test]
+        public void CircleLayer_RadiusNextCircleLayer_ShouldBeIntMaxDistanceFromCenterToInsertedRectangle()
+        {
+            currentLayer = GetLayerAfterFewInsertionsRectangleWithSameSize(currentLayer, 3);
+            var nextRectangleLocation = GetCorrectRectangleLocationByExpectedSector(GetSectorByInsertionsCount(4));
+
+            var nextLayer = currentLayer.OnSuccessInsertRectangle(new Rectangle(nextRectangleLocation, new Size(2,2)));
+
+            nextLayer.Radius.Should().Be(10);
+        }
+
+        private CircleLayer GetLayerAfterFewInsertionsRectangleWithSameSize(CircleLayer layer, int insertionsCount)
+        {
+            for (var i = 0; i < insertionsCount; i++)
+            {
+                var location = GetCorrectRectangleLocationByExpectedSector(GetSectorByInsertionsCount(i));
+                var rectangleForInsert = new Rectangle(location, defaultRectangleSize);
+                layer.OnSuccessInsertRectangle(rectangleForInsert);
+            }
+            return layer;
+        }
+
+        private Sector GetSectorByInsertionsCount(int count)
+        {
+            return (Sector)((count - 1) % 4);
+        }
+
+        private Point GetCorrectRectangleLocationByExpectedSector(Sector s)
+        {
+            switch (s)
+            {
+                case Sector.Top_Right: 
+                    return new Point(currentLayer.Center.X, currentLayer.Center.Y - currentLayer.Radius - defaultRectangleSize.Height);
+                case Sector.Bottom_Right: 
+                    return new Point(currentLayer.Center.X + currentLayer.Radius, currentLayer.Center.Y);
+                case Sector.Bottom_Left: 
+                    return new Point(currentLayer.Center.X - defaultRectangleSize.Width, currentLayer.Center.Y + currentLayer.Radius);
+                default: 
+                    return new Point(currentLayer.Center.X - currentLayer.Radius - defaultRectangleSize.Width,
+                    currentLayer.Center.Y - defaultRectangleSize.Height);
+            }
+        }
+
+        [Test]
+        public void CircleLayer_RectangleWithNewPositionAfterIntersection_ShouldNotIntersectSameRectangle()
+        {
+            var rectangleForInsertion = new Rectangle(new Point(5, -1), new Size(5, 1));
+            var intersectedRectangle = new Rectangle(new Point(8, -6), new Size(8, 7));
+
+            var newPosition = currentLayer.GetRectanglePositionWithoutIntersection(rectangleForInsertion, intersectedRectangle);
+
+            new Rectangle(newPosition, rectangleForInsertion.Size).IntersectsWith(intersectedRectangle).Should()
+                .BeFalse();
+        }
+
+        [Test]
+        public void GetPositionOnCircleWithoutIntersection_ShouldPlaceBottomLeftCornerOnCircle_WhenFoundIntersectionInTopRightSector_AndIntersectedRectangleCanPlaceWithIntCoordinate()
+        {
+            var rectangleForInsertion = new Rectangle(new Point(5, -1), new Size(5, 1));
+            var intersectedRectangle = new Rectangle(new Point(8, -6), new Size(8, 7));
+
+            var newPosition = currentLayer.GetRectanglePositionWithoutIntersection(rectangleForInsertion, intersectedRectangle);
+            var bottomLeftCorner = new Point(newPosition.X, newPosition.Y + intersectedRectangle.Height);
+
+            CurrentLayerContainsPoint(bottomLeftCorner).Should().BeTrue();
+        }
+
+        [Test]
+        public void GetPositionOnCircleWithoutIntersection_ReturnCorrectRectanglePosition_WhenFoundIntersectionInTopRightSector()
+        {
+            var rectangleForInsertion = new Rectangle(new Point(5, -1), new Size(5, 1));
+            var intersectedRectangle = new Rectangle(new Point(8, -6), new Size(8, 7));
+            var expected = new Point(9, 1);
+
+            var newPosition = currentLayer.GetRectanglePositionWithoutIntersection(rectangleForInsertion, intersectedRectangle);
+
+            newPosition.Should().Be(expected);
+        }
+
+
+        [Test]
+        public void GetPositionOnCircleWithoutIntersection_ReturnCorrectRectanglePosition_WhenFoundIntersectionInBottomRightSector()
+        {
+            var rectangleForInsertion = new Rectangle(new Point(8, 9), new Size(5, 1));
+            var intersectedRectangle = new Rectangle(new Point(10, 5), new Size(8, 7));
+            currentLayer = GetLayerAfterFewInsertionsRectangleWithSameSize(currentLayer, 1);
+            var expected = new Point(5, 10);
+
+            var newPosition = currentLayer.GetRectanglePositionWithoutIntersection(rectangleForInsertion, intersectedRectangle);
+
+            newPosition.Should().Be(expected);
+        }
+
+
+        [Test]
+        public void GetPositionOnCircleWithoutIntersection_ReturnCorrectRectanglePosition_WhenFoundIntersectionInBottomLeftSector()
+        {
+            var rectangleForInsertion = new Rectangle(new Point(-3, 9), new Size(5, 3));
+            var intersectedRectangle = new Rectangle(new Point(-7, 8), new Size(8, 7));
+            currentLayer = GetLayerAfterFewInsertionsRectangleWithSameSize(currentLayer, 2);
+            var expected = new Point(-5, 5);
+
+            var newPosition = currentLayer.GetRectanglePositionWithoutIntersection(rectangleForInsertion, intersectedRectangle);
+
+            newPosition.Should().Be(expected);
+        }
+
+        [Test]
+        public void GetPositionOnCircleWithoutIntersection_ReturnCorrectRectanglePosition_WhenFoundIntersectionInTopLeftSector()
+        {
+            var rectangleForInsertion = new Rectangle(new Point(-3, -2), new Size(4, 3));
+            var intersectedRectangle = new Rectangle(new Point(-7, 1), new Size(8, 7));
+            currentLayer = GetLayerAfterFewInsertionsRectangleWithSameSize(currentLayer, 3);
+            var expected = new Point(1, -3);
+
+            var newPosition = currentLayer.GetRectanglePositionWithoutIntersection(rectangleForInsertion, intersectedRectangle);
+
+            newPosition.Should().Be(expected);
+        }
+
+        private bool CurrentLayerContainsPoint(Point p)
+        {
+            return (p.X - currentLayer.Center.X) * (p.X - currentLayer.Center.X) +
+                (p.Y - currentLayer.Center.Y) * (p.Y - currentLayer.Center.Y) == currentLayer.Radius * currentLayer.Radius;
+        }
+    }
+}
