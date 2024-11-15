fe8c9fee10ee03bc99457182c9a44f0645396529
SomEnaMeforme <rushkova909@gmail.com>
2024-11-15 16:57:20 +0500

    Refactoring: add RectangleStorage

---
+++ cs/TagsCloudVisualization/RectangleStorage.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class RectangleStorage
+{
+    private readonly List<RectangleWrapper> elements = new();
+
+    public int Add(Rectangle r)
+    {
+        elements.Add(r);
+        return elements.Count - 1;
+    }
+
+    public IEnumerable<Rectangle> GetAll()
+    {
+        foreach (var rectangle in elements) yield return rectangle;
+    }
+
+    public RectangleWrapper GetById(int id)
+    {
+        return elements[id];
+    }
+
+    public class RectangleWrapper
+    {
+        public RectangleWrapper(Rectangle v)
+        {
+            Value = v;
+        }
+
+        private Rectangle Value { get; set; }
+
+        public Size Size
+        {
+            get => Value.Size;
+            set => Value = new Rectangle(Location, value);
+        }
+
+        public Point Location
+        {
+            get => Value.Location;
+            set => Value = new Rectangle(value, Size);
+        }
+
+        public int Top => Value.Top;
+        public int Bottom => Value.Bottom;
+        public int Left => Value.Left;
+        public int Right => Value.Right;
+
+        public static implicit operator RectangleWrapper(Rectangle v)
+        {
+            return new RectangleWrapper(v);
+        }
+
+        public static implicit operator Rectangle(RectangleWrapper r)
+        {
+            return r.Value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return (obj as RectangleWrapper)?.Value.Equals(Value) ?? (obj as Rectangle?)?.Equals(Value) ?? false;
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/RectangleStorageTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization.Tests
+{
+    public class RectangleStorageTests
+    {
+        private RectangleStorage structStorage;
+        private Rectangle defaulRectangle;
+        [SetUp]
+        public void SetUp()
+        {
+            structStorage = new RectangleStorage();
+            defaulRectangle = new(new(2, 2), new(2, 2));
+        }
+
+        [Test]
+        public void GetRectangles_ShouldGetAllRectangle()
+        {
+            structStorage.Add(defaulRectangle);
+            structStorage.Add(defaulRectangle);
+
+            structStorage.GetAll().Should().HaveCount(2);
+        }
+
+        [Test]
+        public void AddRectangle_ShouldGetIdForRectangle()
+        {
+            var id = structStorage.Add(defaulRectangle);
+
+            structStorage.GetById(id).Should().Be(defaulRectangle);
+        }
+
+        [Test]
+        public void ChangeRectangle_ShouldChangeRectangleByIndex()
+        {
+            var id = structStorage.Add(defaulRectangle);
+            var rectangleForChange = structStorage.GetById(id);
+
+            rectangleForChange.Size = new Size(1, 1);
+
+            structStorage.GetById(id).Size.Should().Be(rectangleForChange.Size);
+        }
+    }
+}
