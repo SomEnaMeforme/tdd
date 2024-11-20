3eb403a025ca942a9adf25b3167de52eb02d2372
SomEnaMeforme <rushkova909@gmail.com>
2024-11-20 22:25:08 +0500

    Fix visualizer and add .csproj

---
+++ cs/TagsCloudVisualization/CircularCloudVisualizer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudVisualizer
+    {
+        private readonly Color backgroundColor = Color.White;
+        private readonly Color rectangleColor = Color.DarkBlue;
+        private readonly Size imageSize;
+        private readonly RectangleStorage rectangleStorage;
+
+        public CircularCloudVisualizer(RectangleStorage rectangles, Size imageSize)
+        {
+            rectangleStorage = rectangles;
+            this.imageSize = imageSize;
+        }
+
+        public void CreateImage(string? filePath = null, bool withSaveSteps = false)
+        {
+            var rectangles = NormalizeSizes(rectangleStorage.GetAll());
+
+            using var image = new Bitmap(imageSize.Width, imageSize.Height);
+            using var graphics = Graphics.FromImage(image);
+            graphics.Clear(backgroundColor);
+            var pen = new Pen(rectangleColor);
+            
+            for (var i = 0; i < rectangles.Length; i++)
+            {
+                var nextRectangle = rectangles[i];
+                graphics.DrawRectangle(pen, nextRectangle);
+                if (withSaveSteps)
+                {
+                    SaveImage(image, $"{filePath}Step{1}");
+                }
+            }
+            SaveImage(image, filePath);
+        }
+
+        private void SaveImage(Bitmap image, string? filePath = null)
+        {
+            var rnd = new Random();
+            filePath = filePath ?? Path.Combine(Path.GetTempPath(), $"testImage{rnd.Next()}.png");
+            image.Save(filePath, ImageFormat.Png);
+        }
+
+        private Rectangle[] NormalizeSizes(IEnumerable<Rectangle> source)
+        {
+            var sourceToArray = source.ToArray();
+
+            var XLength = sourceToArray.Max(r => r.Right) - sourceToArray.Min(r => r.Left);
+            var YLength = sourceToArray.Max(r => r.Bottom) - sourceToArray.Min(r => r.Top);
+
+            var factorX = GetNormalizeFactorByAxis(imageSize.Width, XLength);
+            var factorY = GetNormalizeFactorByAxis(imageSize.Height, YLength);
+
+            return sourceToArray.Select(r => new Rectangle(
+                    new Point(r.X * factorX, r.Y * factorY),
+                    new Size(r.Width * factorX, r.Height * factorY)))
+                .ToArray();
+        }
+
+        private int GetNormalizeFactorByAxis(int imageSizeOnAxis, int rectanglesSizeOnAxis)
+        {
+            const int boundShift = 10;
+            double imageSizeOnAxisWithShiftForBounds = imageSizeOnAxis - boundShift;
+            var stretchFactor = (int)Math.Floor(imageSizeOnAxisWithShiftForBounds / rectanglesSizeOnAxis);
+            return imageSizeOnAxisWithShiftForBounds > rectanglesSizeOnAxis ? stretchFactor : 1;
+        }
+    }
+}
